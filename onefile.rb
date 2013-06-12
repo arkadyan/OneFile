ea69e6e1606f8bf9eaa957a64371f5b7ae5c08ae
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2013-06-12 12:20:11 -0400

    Method to get the html for the requested URL.

--- onefile.rb
+++ onefile.rb
@@ -1,9 +1,29 @@
 #!/usr/bin/env ruby -wKU
 
+require 'net/http'
+require 'uri'
+
+
+
+def get(url)
+  url = URI.parse(url) if url.is_a? String
+
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true if url.scheme == 'https'
+
+  request = Net::HTTP::Get.new(url.request_uri)
+  response = http.request(request)
+  response.body
+end
+
+
+
 url = ARGV.shift
 
 unless url and url.match(/^https?:\/\//)
   raise ArgumentError, 'A valid URL is required.'
 end
 
-puts url
+html = get url
+
+puts html
